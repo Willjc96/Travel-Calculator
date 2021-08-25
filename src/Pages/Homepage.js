16d552a2c2714a5ff70673aadda00baba2d7bfe6
willjc96 <williamjamescook@aol.com>
2021-08-25 12:11:27 +0100

    initial set up of file

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Input } from '@material-ui/core'
+
+export default function Homepage() {
+    return (
+        <div>
+            <p>test</p>
+            <Input/>
+        </div>
+    )
+}
